4457bd5837c8edfef7704309d8b9c37f82af72f1
Augusto <augustocesarrezende64@gmail.com>
2025-03-29 08:29:36 -0300

    primeiro commit

--- api-base/src/main/java/com/example/api_base/model/User.java
+++ api-base/src/main/java/com/example/api_base/model/User.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(generator = "UUID")
     @Column(columnDefinition = "BINARY(16)")
